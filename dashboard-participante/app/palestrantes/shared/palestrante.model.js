f2a5d45c257aff488e2a35d747087ed27ce1c51d
Lucas Mafra <lucas.mafra.barros@usp.br>
2017-03-26 13:35:22 -0300

    fixed untracked files

--- dashboard-participante/app/palestrantes/shared/palestrante.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Palestrante = (function () {
-    function Palestrante() {
-    }
-    return Palestrante;
-}());
-exports.Palestrante = Palestrante;
-//# sourceMappingURL=palestrante.model.js.map
