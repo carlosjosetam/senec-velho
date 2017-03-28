1eda82506cf38aaacdc160224602e1510864b91f
Lucas Mafra <lucas.mafra.barros@usp.br>
2017-03-27 23:20:03 -0300

    removi alguns itens da sidenav

--- dashboard-admin/app/dashboard/core/sidenav/sidenav.component.ts
+++ dashboard-admin/app/dashboard/core/sidenav/sidenav.component.ts
@@ -14,8 +14,6 @@ export class SidenavComponent {
 	menuItems = [
 		{'descricao': 'Atividades', 'link': 'atividades', 'icon': 'dashboard', 'category':''},
 		{'descricao': 'Participantes', 'link': 'participantes', 'icon': 'school','category':''},
-		{'descricao': 'Palestrantes', 'link': 'palestrantes','icon': 'people','category':''},
-		{'descricao': 'Patrocinadores', 'link': 'patrocinadores','icon': 'work','category':''},
 		{'descricao': 'Administradores', 'link': 'administradores','icon': 'assignment_ind','category':''},
 		{'descricao': 'Cadastros', 'link': 'analytics/cadastros','icon': 'trending_up','category':'Analytics'},
 		{'descricao': 'Dados de Acesso', 'link': 'analytics/acesso','icon': 'swap_vert','category':'Analytics'},
